ada18fda67c53ee5379c0e1bea076ddb737d65cd
siracoj <siraco.jared@gmail.com>
2014-04-11 16:07:44 -0400

    display

---
+++ MainDisplay.cu
@@ -0,0 +1,22 @@
+#include <cv.h>
+#include <highgui.h>
+
+void main(int argc,char *argv[])
+{
+    int c;
+    IplImage* color_img;
+    CvCapture* cv_cap = cvCaptureFromCAM(0);
+    cvNamedWindow("Video",0); // create window
+    for(;;) {
+        color_img = cvQueryFrame(cv_cap); // get frame
+        if(color_img != 0)
+            cvShowImage("Video", color_img); // show frame
+        c = cvWaitKey(10); // wait 10 ms or for key stroke
+        if(c == 27)
+            break; // if ESC, break and quit
+    }
+    /* clean up */
+    cvReleaseCapture( &cv_cap );
+    cvDestroyWindow("Video");
+}
+
